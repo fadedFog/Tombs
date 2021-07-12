f5765a9b9318487ba3381a8842291c20b99a74b7
Fog <Fog@DESKTOP-P4FM27T>
2021-07-12 13:56:21 +0300

    Delete all new changes about spring data

--- app/src/main/java/ru/fadedfog/tombs/repository/CustomizedStatistics.java
+++
@@ -1,9 +0,0 @@
-package ru.fadedfog.tombs.repository;
-
-public interface CustomizedStatistics<T> {
-
-	void createInitLineStatistics();
-	
-	void updateStatistics(int updateNumberSteps);
-	
-}
--- app/src/main/java/ru/fadedfog/tombs/service/ServiceStatisticsCollector.java
+++ app/src/main/java/ru/fadedfog/tombs/service/ServiceStatisticsCollector.java
@@ -1,22 +1,16 @@
 package ru.fadedfog.tombs.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import ru.fadedfog.tombs.repository.StatisticsRepository;
 
 @Service
-@Component
+@Component 
 public class ServiceStatisticsCollector {
-	@Autowired
-	private StatisticsRepository statisticsRepository;
 	private int numberSteps;
 
 	public void saveNumberSteps() {
-		statisticsRepository.updateStatistics(numberSteps);
 	}
 	
 	public int getNumberSteps() {
