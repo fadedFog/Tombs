d76fa0ea6b55283f90e7ce709c84b42ec6b1c46b
Fog <Fog@DESKTOP-P4FM27T>
2021-07-13 21:24:38 +0300

    Remove file properties, added DatasourceConfig. Problem spring component

--- app/src/main/java/ru/fadedfog/tombs/repository/StatisticsRepository.java
+++ app/src/main/java/ru/fadedfog/tombs/repository/StatisticsRepository.java
@@ -6,9 +6,9 @@ import ru.fadedfog.tombs.model.Statistics;
 
 public interface StatisticsRepository extends CrudRepository<Statistics, Long>  {
 
-	void save();
+//		void save();
 	
-	void updateById(long id, int stepsUser);
+//	void updateById(long id, int stepsUser);
 
 	Statistics findById(long id);
 	
